e776b9156e181d7f031f9d3d51ac6562112e8564
Javier Rodríguez <frodsot2402@g.educaand.es>
2024-09-25 09:34:16 +0200

    Prueba componente

---
+++ src/app/hijo/hijo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hijo',
+  standalone: true,
+  imports: [],
+  templateUrl: './hijo.component.html',
+  styleUrl: './hijo.component.css'
+})
+export class HijoComponent {
+
+}
